853dcdf3fc4ae6a1bbf427c8e103b50c567e9e17
“songy00n” <allnu@naver.com>
2023-06-09 10:17:23 +0900

    jsp

--- JspPro1/src/main/java/VoteDB/VoteController.java
+++ JspPro1/src/main/java/VoteDB/VoteController.java
@@ -12,7 +12,10 @@ import javax.servlet.http.HttpServletResponse;
 
 /**
  * Servlet implementation class VoteController
- * 컨트롤러의 역할:뷰랑 모델을 연결, 요청을 받아서 모델로 분기시킴
+ * 컨트롤러 작성하는 부분
+ * View와 Model 연결하기 
+ * 요청을 받아서 Model 분기시키기
+ * 
  */
 @WebServlet("/VoteController")
 public class VoteController extends HttpServlet {
@@ -23,73 +26,64 @@ public class VoteController extends HttpServlet {
      */
     public VoteController() {
         super();
+        // TODO Auto-generated constructor stub
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
 		request.setCharacterEncoding("UTF-8");
-		try {
-			requestPro(request, response);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		requestPro( request,  response );
 	}
 
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		request.setCharacterEncoding("UTF-8");
-		try {
-			requestPro(request, response);
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
+		requestPro( request,  response );
 	}
 	
-
-	private void requestPro(HttpServletRequest request, HttpServletResponse response) throws Exception {
-		// 요청한 것을 받아서 모델 분기하기
-		// 뷰 보내기
-		String uri = request.getRequestURI(); // 요청한 브라우저의 URI값을 얻는다
-		String context = request.getContextPath(); //요청한 context 경로를 얻음
+	private void requestPro(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//요청한 것을 받아서 Model 분기하기 
+		// View 보내기 
+		String uri = request.getRequestURI(); //요청한 브라우저의 URI값을 얻는다.
+		String context = request.getContextPath();// 요청한 context경로를 얻는다.
 		String command = uri.substring(context.length());
 		String site = null;
 		
-		System.out.println("uri: " + uri);
-		System.out.println("context" + context);
-		System.out.println("command" + command);
-		
+		System.out.println(  "uri : " + uri );
+		System.out.println(  "context : " + context );
+		System.out.println(  "command : " + command );
+        
 		VoteDAO vote = new VoteDAO();
 		
-		switch(command) {
-			case "/main.do": site="index.jsp"; break;
-			case "/memberList.do": site="vote.selectmemberList.jsp"; break;
-			case "/voteMember.do": site="voteMember.jsp"; break;
-			case "/voteList.do": site="voteList.jsp"; break;
-			case "/vote.do": 
-				 int result = vote.insertVote(request,response); // 삽입할 메소드로 호출함 
-				 response.setContentType("text/html; charset=UTF-8");
-				 PrintWriter out = response.getWriter();
-				 if(result==1) {
-					 out.print("<script>");
-					 out.print("alert('투표하기 정보가 정상적으로 등록되었습니다'); location.href='" + context + "';");
-					 out.println("</script>");
-					 out.flush();
-				 } else {
-					 out.println();
-				 }
-				 break;
-			default: break;
+		switch( command ) {
+		    case "/main.do":   site="index.jsp"; break;
+		    case "/memberList.do":   
+		    	    site = vote.selectMember(request, response);//검색할 DAO에 있는 메소드를 호출한다.
+		    	    break; //후보조회
+		    case "/voteMember.do":   site="voteMember.jsp"; break;
+		    case "/voteList.do":   site="voteList.jsp"; break;
+		    case "/vote.do" : 
+		    	     int result  = vote.insertVote( request, response );//삽입할 메소드로 호출합니다. 
+		    	     response.setContentType("text/html; charset=UTF-8");
+		    	     PrintWriter out = response.getWriter();
+		    	     if(result == 1) {
+		    	 		out.println("<script>");
+		    	 		out.println("alert('투표하기 정보가 정상적으로 등록 되었습니다!'); location.href='"+context+"'; ");
+		    	 		out.println("</script>");
+		    	 		out.flush();
+		    	 	}else {
+		    	 		out.println("<script>");
+		    	 		out.println("alert('등록실패!'); location.href='"+context+"'; ");
+		    	 		out.println("</script>");
+		    	 		out.flush();
+		    	 	}	
+		    	     break;
+		    default:  break;
 		}
+		
 		RequestDispatcher dispatcher = request.getRequestDispatcher(site);
 		dispatcher.forward(request, response);
-	
 	}
-
 }
--- JspPro1/src/main/java/VoteDB/VoteDAO.java
+++ JspPro1/src/main/java/VoteDB/VoteDAO.java
@@ -4,38 +4,44 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import VoteBean.Member;
+
 public class VoteDAO {
-	Connection conn = null; // 변수선언 DB와 연결 
-	PreparedStatement ps = null; // sql문 담당 
-	ResultSet rs = null; // select문에서 검색 결과를 담을 
-	
-	public static Connection getConnection() throws Exception {
-		Class.forName("oracle.jdbc.OracleDriver"); // oracle 드라이버 JVM에 로딩
-		Connection con = 
-		DriverManager.getConnection("jdbc:oracle:thin@//localhost:1521/xe","system","1234");
-		// 2단계 연결하기
-		return con;
-	}
+    Connection conn = null; //변수선언  DB와 연결
+    PreparedStatement ps = null;     //  SQL문 담당
+    ResultSet rs = null;// select문에서 검색 결과를 담을 것
+    
+    public static Connection getConnection() throws Exception {
+    	 Class.forName("oracle.jdbc.OracleDriver"); //1단계 오라클 드라이버 JVM에 로딩
+    	 Connection con  =
+    	     DriverManager.getConnection("jdbc:oracle:thin:@//localhost:1521/xe", "system", "1234");
+    	 //2단계 연결하기
+    	 return con;
+     }
 
 	public int insertVote(HttpServletRequest request, HttpServletResponse response)  {
-		// 투표하기 삽입 DAO
-		int result = 0;
+		//투표하기 삽입 DAO
+		int result =0;
 		try {
 			conn = getConnection();
-			// 2단계 브라우저에 입력한 자료를 자바로 가지고 옴 
+			//1단계  DB연결
+	        //2단계 브라우저에 입력한 자료를 자바로 가지고 온다
 			String v_jumin = request.getParameter("v_jumin");
 			String v_name = request.getParameter("v_name");
 			String m_no = request.getParameter("m_no");
 			String v_time = request.getParameter("v_time");
 			String v_area = request.getParameter("v_area");
 			String v_confirm = request.getParameter("v_confirm");
-			// 3단계 sql문의 삽입문을 작성 
-			String sql = "insert into tbl_vote_202005 values (?,?,?,?,?,?)";
+			//3단계 SQL문의 삽입문을 작성한다
+			String sql = "insert into tbl_vote_202005 values ( ?, ?, ? , ?, ?, ?)";
 			ps = conn.prepareStatement(sql);
+			
 			ps.setString(1, v_jumin);
 			ps.setString(2, v_name);
 			ps.setString(3, m_no);
@@ -43,16 +49,72 @@ public class VoteDAO {
 			ps.setString(5, v_area);
 			ps.setString(6, v_confirm);
 			
-			// 4단계 실행시킴
-			result = ps.executeUpdate(); // 실패 0,성공 1
-			System.out.println("성공 실패 여부 " + result);
-			// 5단계 자원 반납
-		try {	conn.close();
-			ps.close();
+			//4단계 실행시킨다
+			result = ps.executeUpdate(); //실패 0, 성공 1
+			System.out.println("성공 실패 여부 "  + result);
+	
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
-		} // 1단계 DB 연결 
+		} finally {
+			//5단계 자원반납
+			try {
+				ps.close();
+				conn.close();
+			} catch (SQLException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			
+		}
+		return result;
 	}
-	return result;
+
+	public String selectMember(HttpServletRequest request, HttpServletResponse response) {
+		// 후보조회 
+	    ArrayList<Member> alist = new ArrayList<Member>();
+	    try {
+			conn = getConnection();
+			String sql = "SELECT ";  
+		       sql+= " M.m_no, ";
+		       sql+= " M.m_name, ";
+		       sql+= " P.p_name, ";
+		       sql+= " DECODE(M.p_school,'1','고졸','2','학사','3','석사','박사') p_school, ";
+		       sql+= " substr(M.m_jumin,1,6)|| ";
+		       sql+= " '-'||substr(M.m_jumin,7) m_jumin, ";
+		       sql+= " M.m_city, ";
+		       sql+= " substr(P.p_tel1,1,2)||'-'||P.p_tel2||'-'||";
+		       sql+= " (substr(P.p_tel3,4)||";
+		       sql+= "  substr(P.p_tel3,4)||";
+		       sql+= "  substr(P.p_tel3,4)||";
+		       sql+= "  substr(P.p_tel3,4)) p_tel ";
+		       sql+= " FROM tbl_member_202005 M, tbl_party_202005 P ";
+		       sql+= " WHERE M.p_code = P.p_code";
+		       
+		       ps = conn.prepareStatement(sql);
+		       rs = ps.executeQuery();
+		       
+		       while( rs.next() ) {
+		    	      Member member = new Member(); //DTO
+		    	      member.setM_no(rs.getString(1));
+		  			 member.setM_name(rs.getString(2));
+		  		 	member.setP_name(rs.getString(3));
+		  			member.setP_school(rs.getString(4));
+		  			member.setM_jumin(rs.getString(5));
+		  			member.setM_city(rs.getString(6));
+		  			member.setP_tel(rs.getString(7));
+		  			
+		  			alist.add(member);
+		  			
+		       }
+		       request.setAttribute("list", alist);
+		       rs.close();
+		       ps.close();
+		       conn.close();
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return "memberList.jsp";
+	}
 }
